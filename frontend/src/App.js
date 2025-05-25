0a6ef2893df23d4a901c289972d83f1cf3ff2cf6
Mangal Kumar <mangal192004@gmail.com>
2025-05-25 21:29:47 +0530

    1.1.0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      
+      path(route='login', view=views.login_user, name='login'),
+      path('login/', TemplateView.as_view(template_name="Home.html")),
     </Routes>
   );
 }
